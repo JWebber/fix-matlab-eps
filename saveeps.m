60913fbae5c3e5ad0211390323c2384e32fe446d
Joseph Webber <josephwebber@outlook.com>
2022-02-07 10:28:10 +0000

    Update saveeps.m
    Prevents System.IO.IOException when the file is loaded whilst still being used by Matlab

--- saveeps.m
+++ saveeps.m
@@ -1,5 +1,6 @@
 function saveeps(filename)
     print('-depsc2', filename, '-painters');
+    fclose('all'); % Prevent IO exceptions
     system(strcat("PATH_TO_FIXMATLABEPS.EXE HERE """, filename, """"));
 end
 
